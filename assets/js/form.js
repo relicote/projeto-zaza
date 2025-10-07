8942177e7fecc4b0b2c784e3fc64583c83eb338a
relicote <relicote@gmail.com>
2025-10-06 21:45:54 -0300

    Corrige bug de form não funcionar mensagem de ok

--- assets/js/form.js
+++ assets/js/form.js
@@ -11,33 +11,13 @@ class FormSubmit{
 
     displaySuccess(){
         this.form.innerHTML = this.settings.sucess;
+        console.log("sucesso", this.settings.sucess);
     }
 
     displayError() {
         this.form.innerHTML = this.settings.error;
+        console.log("erro", this.settings.error);
     }
-
-//     async sendForm(){
-//         try{
-//         await fetch(this.url, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             Accept: "application/json",
-//         },
-//         body:"",
-//         });
-
-//         this.displaySucess();
-//     } catch (error) {
-//         this.displayError();
-//         throw new Error(error);
-//     }
-// }
-    // init(){
-    //     if (this.form) this.buttonforms.addEventListener("click", () => this.displaySucess());
-    //     return this;
-    // }
 }
 
 const FormSubmit = new FormSubmit({
--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -56,19 +56,13 @@
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
     .then(response => {
-      if( response.ok ) {
-        return response.text()
+      if( response.status === 200 ) {
+         thisForm.querySelector('.loading').classList.remove('d-block');
+         thisForm.querySelector('.sent-message').classList.add('d-block');
+        thisForm.reset(); 
+        return "Ok!";
       } else {
         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
     })
     .catch((error) => {
